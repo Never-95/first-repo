b4e2b9b06ca9692df9fbec695a0b300ebfed2a00
Never-95 <mattsnever95@outlook.com>
2024-02-29 09:33:50 +0000

    printhello

---
+++ printhello.java
@@ -0,0 +1,13 @@
+class printhello{
+	public static void main(String[] a){
+		helloMain();
+		return;
+	}
+
+	public static void helloMain(){
+		for (int i=0; i<10; i++){
+			System.out.println("Hello world");
+		}
+		return;
+	}
+}
